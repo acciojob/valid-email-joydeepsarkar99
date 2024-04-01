682da498a151cbca59f7ee6e55ea05e8b91f042c
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-01 19:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function validEmail(str) {
-	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-	if (str.value.match(validRegex)) {
-		return true
-	} 
-	return false;
+	let flag = false; 
+	if(str.length<5){
+		flag = false;
+	}else if(str[0] === "@" || str[str.length-1] === "@" || str[0] === "." || str[str.length-1] === "."){
+		flag =  false;
+	}else if(str.includes("@") && str.includes(".")){
+		flag = true;
+	}else{
+		flag = false;
+	}
+	return flag;
 }
 
-// Do not change the code below.
+Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
 
