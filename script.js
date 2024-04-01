41a7ec0d4fdebc7ccbfa20db849bb45730894a9d
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-01 18:58:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function validEmail(str) {
-  //your JS code here.
+	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	if (str.value.match(validRegex)) {
+		return true
+	} 
+	return false;
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
